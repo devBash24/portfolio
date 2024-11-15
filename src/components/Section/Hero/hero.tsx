a657b8963abbdb18a71459eed6a24823ebf84f30
Bash John <138396242+devBash24@users.noreply.github.com>
2024-11-14 22:05:18 -0400

    change padding on cta button

--- src/components/Section/Hero/hero.tsx
+++ src/components/Section/Hero/hero.tsx
@@ -71,7 +71,7 @@ const Hero = () => {
           <motion.button
             variants={fadeInVariant}
             custom={3}
-            className="bg-accent hover:bg-opacity-80 text-white font-bold py-4 px-8 text-lg rounded shadow-cta transition-colors"
+            className="bg-accent hover:bg-opacity-80 text-white font-bold py-2 px-4 text-lg rounded shadow-cta transition-colors"
             onClick={() => window.open("https://github.com/devBash24")}
           >
             {t("hero.cta")}
@@ -80,7 +80,7 @@ const Hero = () => {
           <motion.button
             variants={fadeInVariant}
             custom={4}
-            className="bg-accent hover:bg-opacity-80 text-white font-bold py-4 px-8 text-lg rounded shadow-cta transition-colors"
+            className="bg-accent hover:bg-opacity-80 text-white font-bold py-2 px-4 text-lg rounded shadow-cta transition-colors"
             onClick={() =>
               onSectionChange("contact")
             }
